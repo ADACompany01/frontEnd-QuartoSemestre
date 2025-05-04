93cab8b654060d6b8b888f62f61a59b5e8e22a99
SamuelPorcer <samuelpregnolatto@gmail.com>
2025-05-04 14:36:30 -0300

    fix(frontend): corrige importações em SignUpFuncionario.jsx

--- src/components/sign-up-funcionario/SignUpFuncionario.jsx
+++ src/components/sign-up-funcionario/SignUpFuncionario.jsx
@@ -9,7 +9,6 @@ import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import MuiCard from '@mui/material/Card';
-import MenuItem from '@mui/material/MenuItem';
 import { styled } from '@mui/material/styles';
 import AppTheme from '../shared-theme/AppTheme';
 import { GoogleIcon, FacebookIcon, SitemarkIcon } from './CustomIcons';
