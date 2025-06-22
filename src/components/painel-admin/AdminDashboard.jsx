d72ef6d96a11747f6a2d58410b398639af50c4fe
MayconBasilio <142848115+MayconBasilio@users.noreply.github.com>
2025-06-21 22:45:52 -0300

    fix:(AdminDasboard)
    Não estava aparecendo o dash para o admin

--- src/components/painel-admin/AdminDashboard.jsx
+++ src/components/painel-admin/AdminDashboard.jsx
@@ -5,8 +5,8 @@ const AdminDashboard = () => {
       <iframe
         title="Relatório Power BI"
         width="100%"
-        height="600"
-        src="https://app.powerbi.com/view?r=SEU-LINK-AQUI"
+        height="970px"
+        src="https://app.powerbi.com/reportEmbed?reportId=2898089c-6660-403a-a8bc-b7c2e5aedeab&autoAuth=true&ctid=9f9199ee-21cb-44c4-a206-8134332e517e"
         frameBorder="0"
         allowFullScreen
       ></iframe>
